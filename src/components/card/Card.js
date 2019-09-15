5b50a546adc6efc162c5c4d9c2f71d575d0e8e29
Ryan Browne <brownetown7@gmail.com>
2019-09-15 18:18:42 +0100

    Adds card styles

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,26 +6,27 @@ function Card ({ title, location, image, stars, guests, length, departure, price
 
   return (
     <article className={styles.card}>
-      <div className="card-top">
-        <div className="card-image">
+      <div className={styles.top}>
+        <div className={styles.image}>
           <img src={require(`../../assets/${image}`)} alt="holiday image 1"/>
-          <div className="card-read-more">
-            <a href="#">Read more about this hotel</a>
+          <div className={styles.readmore}>Read more about this hotel</div>
+        </div>
+        <div className={styles.info}>
+          <h2 className={styles.title}>{title}</h2>
+          <h3 className={styles.location}>{location}</h3>
+          <div className={styles.stars}>{stars}</div>
+          <p className={styles.guests}>{guests}</p>
+          <p className={styles.length}>{length}</p>
+          <p className={styles.departure}>Departing from <em>{departure}</em></p>
+          <div className={styles.button}>
+            Book now <br/>
+            <span className={styles.price}>£{price}</span>
           </div>
         </div>
-        <div className="card-info">
-          <h2 className="card-title">{title}</h2>
-          <h3 className="card-location">{location}</h3>
-          <div className="card-stars">{stars}</div>
-          <p className="card-guests">{guests}</p>
-          <p className="card-length">{length}</p>
-          <p className="card-departure">departing from {departure}</p>
-          <div className="button">book now £{price}</div>
-        </div>
       </div>
-      <div className="card-bottom">
+      <div className={styles.bottom}>
         <h4>Overview</h4>
-        <p className="card-description">{description}</p>
+        <p className={styles.description}>{description}</p>
       </div>
     </article>
   );

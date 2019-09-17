2e183657f8dca87fa74ef3d055c9469dd887b367
Ryan Browne <brownetown7@gmail.com>
2019-09-17 10:45:11 +0100

    Fixes up data format and adds some logic

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './Card.module.css';
 
-function Card ({ title, location, image, rating, guests, length, departure, price, description }) {
+function Card ({ title, location, image, rating, guests, length, departureDate,departure, price, description }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -30,9 +30,13 @@ function Card ({ title, location, image, rating, guests, length, departure, pric
         <div className={styles.info}>
           <h2 className={styles.title}>{title}</h2>
           <h3 className={styles.location}>{location}</h3>
-          <div className={styles.stars}>{rating}</div>
-          <p className={styles.guests}>{guests}</p>
-          <p className={styles.length}>{length}</p>
+          <div className={styles.stars} data-stars={rating}></div>
+          <p className={styles.guests}>
+            <strong>{guests.adults}</strong> {guests.adults === 1 ? 'Adult, ' : 'Adults, '}
+            <strong>{guests.children}</strong> {guests.children === 1 ? 'Child, ' : 'Children, '} 
+            {guests.infant ? `& ${guests.infant} infant` : ''}
+          </p>
+          <p className={styles.length}><strong>{departureDate}</strong> for <strong>{length}</strong></p>
           <p className={styles.departure}>Departing from <em>{departure}</em></p>
           <div className={styles.button}>
             Book now <br/>
--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -11,22 +11,14 @@ const destinationsArray = [
     location: "Costa Adeje, Tenerife",
     image: "hotel-image-1.png",
     rating: 5,
-    guests: "2 Adults, 2 Children & 1 Infant",
-    length: "3rd July 2019 for 7 days",
+    guests: {
+      adults: 2,
+      children: 2,
+      infant: 1
+    },
     departureDate: "3rd July 2019",
+    length: "7 days",
     departure: "East Midlands",
-    price: 1136.50,
-    description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea."
-  },
-  {
-    title: "Iberostar Grand Salome 2",
-    location: "Costa Adeje, Tenerife",
-    image: "hotel-image-1.png",
-    rating: 5,
-    guests: "2 Adults, 2 Children & 1 Infant",
-    length: "3rd July 2019 for 7 days",
-    departureDate: "3rd July 2019",
-    departure: "Liverpool",
     price: 1136.50,
     description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea."
   },
@@ -35,9 +27,12 @@ const destinationsArray = [
     location: "Costa Adeje, Tenerife",
     image: "hotel-image-2.png",
     rating: 4,
-    guests: "2 Adults, 1 Child",
-    length: "27th May 2019 for 7 days",
+    guests: {
+      adults: 2,
+      children: 1
+    },
     departureDate: "27th May 2019",
+    length: "7 days",
     departure: "Liverpool",
     price: 696.80,
     description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea."
@@ -47,9 +42,12 @@ const destinationsArray = [
     location: "Costa Adeje, Tenerife",
     image: "hotel-image-3.png",
     rating: 3,
-    guests: "2 Adults, 2 Children",
-    length: "3rd July 2019 for 7 days",
+    guests: {
+      adults: 2,
+      children: 2
+    },
     departureDate: "3rd July 2019",
+    length: "7 days",
     departure: "Manchester",
     price: 499.99,
     description: "The Iberostar Grand Salome has an exceptional location in the south of Tenerife, overlooking the Atlantic Ocean. It is situated between the Golf del Sur and the Amarillo Gold courses, and is an ideal hotel for families, couples and groups who are looking for a holiday full of sport, sun and sea."
@@ -134,6 +132,7 @@ function Container () {
           rating={destination.rating}
           guests={destination.guests}
           length={destination.length}
+          departureDate={destination.departureDate}
           departure={destination.departure}
           price={destination.price}
           description={destination.description}
